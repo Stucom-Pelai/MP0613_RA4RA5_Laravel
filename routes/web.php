0d68961f67eee8be0804a6a3ca8f4122273c014e
Javier G. <xavichavochavuski@gmail.com>
2025-11-25 16:37:34 +0100

    List country and duration data for each film #6

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('welcome');
+});
+
 
 Route::middleware('year')->group(function() {
     Route::group(['prefix'=>'filmout'], function(){
